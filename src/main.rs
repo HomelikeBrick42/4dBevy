382dd1ab88b162574ef4d2b47e2e5dac13a1c142
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2025-05-29 22:07:18 +1200

    added some keyboard event printing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use bevy::prelude::*;
+use bevy::{input::keyboard::KeyboardInput, prelude::*};
 use transform::TransformPlugin;
 
 fn main() -> AppExit {
     App::new()
         .add_plugins((DefaultPlugins, TransformPlugin))
         .add_systems(Startup, setup)
+        .add_systems(Update, print_key_presses)
         .run()
 }
 
 fn setup(commands: Commands) {
     _ = commands;
+}
+
+fn print_key_presses(mut inputs: EventReader<KeyboardInput>) {
+    for input in inputs.read() {
+        info!("{:?} {:?}", input.key_code, input.state);
+    }
 }
