565827a59ced499a3cbf334b6446567a87768d25
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2025-05-29 22:48:55 +1200

    added fps printing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
-use bevy::{input::keyboard::KeyboardInput, prelude::*};
+use bevy::{
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+    input::keyboard::KeyboardInput,
+    prelude::*,
+};
 use transform::TransformPlugin;
 
+const PRINT_FPS: bool = false;
+
 fn main() -> AppExit {
-    App::new()
-        .add_plugins((DefaultPlugins, TransformPlugin))
+    let mut app = App::new();
+
+    if PRINT_FPS {
+        app.add_plugins(FrameTimeDiagnosticsPlugin::default());
+    }
+
+    app.add_plugins((DefaultPlugins, TransformPlugin))
         .add_systems(Startup, setup)
-        .add_systems(Update, print_key_presses)
-        .run()
+        .add_systems(Update, (print_diagnostics, print_key_presses));
+
+    app.run()
 }
 
 fn setup(commands: Commands) {
     _ = commands;
+}
+
+fn print_diagnostics(d: Res<DiagnosticsStore>) {
+    if let Some(fps) = d.get_measurement(&FrameTimeDiagnosticsPlugin::FPS) {
+        info!("FPS: {}", fps.value);
+    }
 }
 
 fn print_key_presses(mut inputs: EventReader<KeyboardInput>) {
